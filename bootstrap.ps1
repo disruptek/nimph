ff3b5599f9eb79ad8537abce1fa385fbe04938f9
Constantine Molchanov <moigagoo@live.com>
2019-12-24 12:00:06 +0400

    Add installation script for Windows in PowerShell.

---
+++ bootstrap.ps1
@@ -0,0 +1,10 @@
+if ( !(Join-Path 'src' 'nimph.nim' | Test-Path) ) {
+  git clone --depth git://github.com/disruptek/nimph.git
+  Set-Location nimph
+} 
+
+$env:NIMBLE_DIR = Resolve-Path 'deps'
+New-Item -Type Directory $env:NIMBLE_DIR -Force | Out-Null
+
+nimble --accept refresh
+nimble install "--passNim:--path:$(Resolve-Path 'src') --outDir:$PWD"
