c87592001f1121e6a5475f1878623ebe1caed1c4
Andy Davidoff <andy+github@disruptek.com>
2020-05-01 11:31:34 -0400

    update hub handling

--- src/nimph/thehub.nim
+++ src/nimph/thehub.nim
@@ -14,6 +14,7 @@ import std/times
 
 import rest
 import github
+import jsonconvert
 
 import nimph/spec
 import nimph/group
@@ -23,11 +24,38 @@ const
 
 type
   HubKind* = enum
+    HubRelease
+    HubTag
+    HubCommit
     HubRepo
     HubIssue
     HubPull
     HubUser
     HubCode
+
+  HubTree* = object
+    sha*: string
+    url*: Uri
+    `type`*: string
+
+  HubContact* = object
+    name*: string
+    email*: string
+    date*: DateTime
+
+  HubVerification* = object
+    verified*: bool
+    reason*: string
+    signature*: string
+    payload*: string
+
+  HubCommitMeta* = object
+    url*: Uri
+    author*: HubContact
+    committer*: HubContact
+    message*: string
+    commentCount*: int
+    tree*: HubTree
 
   HubResult* = ref object
     htmlUrl*: Uri
@@ -38,7 +66,24 @@ type
     state*: string
     name*: string
     user*: HubResult
+    tagName*: string
+    targetCommitish*: string
+    sha*: string
+    created*: DateTime
+    updated*: DateTime
     case kind*: HubKind:
+    of HubCommit:
+      tree*: HubTree
+      author*: HubResult
+      committer*: HubResult
+      parents*: seq[HubTree]
+      commit*: HubCommitMeta
+    of HubTag:
+      tagger*: HubContact
+      `object`*: HubTree
+    of HubRelease:
+      draft*: bool
+      prerelease*: bool
     of HubUser:
       login*: string
     of HubIssue:
@@ -49,7 +94,6 @@ type
     of HubCode:
       path*: string
       repository*: HubResult
-      sha*: string
     of HubRepo:
       fullname*: string
       description*: string
@@ -58,8 +102,6 @@ type
       forks*: int
       owner*: string
       size*: int
-      created*: DateTime
-      updated*: DateTime
       pushed*: DateTime
       issues*: int
       clone*: Uri
@@ -118,68 +160,76 @@ proc findGithubToken*(): Option[string] =
   if token != "":
     result = token.some
 
-proc newHubResult*(kind: HubKind; js: JsonNode): HubResult =
+proc newHubContact*(js: JsonNode): HubContact =
+  ## parse some json into a simple contact record
+  let
+    tz = utc()
+  # 🐼 result = js.to(HubContact)
+  if js == nil or "date" notin js or js.kind != JString:
+    result = HubContact(date: now())
+  else:
+    result = HubContact(
+      date: js["date"].getStr.parse(hubTime, zone = tz)
+    )
+  if js != nil:
+    result.name = js.get("name", "")
+    result.email = js.get("email", "")
+
+proc newHubTree*(js: JsonNode): HubTree =
+  ## parse something like a commit tree
+  result = HubTree()
+  if js != nil:
+    result.url = js.get("url", "").parseUri
+    result.sha = js.get("sha", "")
+    result.`type` = js.get("type", "")
+
+proc newHubCommitMeta*(js: JsonNode): HubCommitMeta =
+  ## collect some ingredients found in a typical commit
+  result = HubCommitMeta(
+    committer: newHubContact js.getOrDefault("committer"),
+    author: newHubContact js.getOrDefault("author")
+  )
+  result.tree = newHubTree js.getOrDefault("tree")
+  result.commentCount = js.get("comment_count", 0)
+
+proc newHubResult*(kind: HubKind; js: JsonNode): HubResult
+
+proc init*(result: var HubResult; js: JsonNode) =
   ## instantiate a new hub object using a jsonnode
 
   # impart a bit of sanity
   if js == nil or js.kind != JObject:
     raise newException(Defect, "nonsensical input: " & js.pretty)
 
-  let
-    tz = utc()
-    kind = block:
-      if "head" in js:
-        HubPull
-      elif kind == HubPull:
-        HubIssue
-      else:
-        kind
-
-  case kind:
+  case result.kind:
+  of HubRelease: discard
+  of HubTag: discard
+  of HubCommit:
+    result.committer = HubUser.newHubResult(js["committer"])
+    result.author = HubUser.newHubResult(js["author"])
+    result.sha = js["sha"].getStr
   of HubIssue:
-    result = HubResult(kind: HubIssue)
-    result.htmlUrl = js["html_url"].getStr.parseUri
     if "closed_by" in js and js["closed_by"].kind == JObject:
       result.closedBy = HubUser.newHubResult(js["closed_by"])
   of HubPull:
-    result = HubResult(kind: HubPull)
-    result.htmlUrl = js["html_url"].getStr.parseUri
     result.merged = js.getOrDefault("merged").getBool
     if "merged_by" in js and js["merged_by"].kind == JObject:
       result.mergedBy = HubUser.newHubResult(js["merged_by"])
   of HubCode:
-    result = HubResult(kind: HubCode)
-    result.htmlUrl = js["html_url"].getStr.parseUri
-    result.path = js["path"].getStr
-    result.sha = js["sha"].getStr
-    result.name = js["name"].getStr
+    result.path = js.get("path", "")
+    result.sha = js.get("sha", "")
+    result.name = js.get("name", "")
     if "repository" in js:
       result.repository = HubRepo.newHubResult(js["repository"])
   of HubRepo:
-    template thenOrNow(label: string): DateTime =
-      if label in js:
-        js[label].getStr.parse(hubTime, zone = tz)
-      else:
-        now()
-    result = HubResult(kind: HubRepo,
-      created: thenOrNow "created_at",
-      updated: thenOrNow "updated_at",
-      pushed: thenOrNow "pushed_at",
-    )
-    result.htmlUrl = js["html_url"].getStr.parseUri
-    result.fullname = js["full_name"].getStr
-    if "owner" in js and js["owner"].kind == JString:
-      result.owner = js["owner"].getStr
-    result.name = js["name"].getStr
-    result.description = js["description"].getStr
-    if "stargazers_count" in js:
-      result.stars = js["stargazers_count"].getInt
-    if "subscriber_count" in js:
-      result.watchers = js.getOrDefault("subscriber_count").getInt
-    if "forks_count" in js:
-      result.forks = js["forks_count"].getInt
-    if "open_issues_count" in js:
-      result.issues = js["open_issues_count"].getInt
+    result.fullname = js.get("full_name", "")
+    result.owner = js.get("owner", "")
+    result.name = js.get("name", "")
+    result.description = js.get("description", "")
+    result.stars = js.get("stargazers_count", 0)
+    result.watchers = js.get("subscriber_count", 0)
+    result.forks = js.get("forks_count", 0)
+    result.issues = js.get("open_issues_count", 0)
     if "clone_url" in js:
       result.clone = js["clone_url"].getStr.parseUri
     if "git_url" in js:
@@ -192,22 +242,81 @@ proc newHubResult*(kind: HubKind; js: JsonNode): HubResult =
       result.web = result.htmlUrl
     if "license" in js:
       result.license = js["license"].getOrDefault("name").getStr
-    if "default_branch" in js:
-      result.branch = js["default_branch"].getStr
-    result.original = not js["fork"].getBool
-    result.score = js.getOrDefault("score").getFloat
+    result.branch = js.get("default_branch", "")
+    result.original = not js.get("fork", false)
+    result.score = js.get("score", 0.0)
   of HubUser:
-    result = HubResult(kind: HubUser)
-    result.login = js["login"].getStr
-  if "id" in js:
-    result.id = js["id"].getInt
+    result.login = js.get("login", "")
+  result.id = js.get("id", 0)
   if "title" in js:
-    result.body = js["body"].getStr
-    result.number = js["number"].getInt
-    result.title = js["title"].getStr
-    result.state = js["state"].getStr
+    result.body = js.get("body", "")
+    result.title = js.get("title", "")
+    result.state = js.get("state", "")
+    result.number = js.get("number", 0)
   if "user" in js and js["user"].kind == JObject:
     result.user = HubUser.newHubResult(js["user"])
+
+proc newHubResult*(kind: HubKind; js: JsonNode): HubResult =
+  # impart a bit of sanity
+  if js == nil or js.kind != JObject:
+    raise newException(Defect, "nonsensical input: " & js.pretty)
+
+  template thenOrNow(label: string): DateTime =
+    if js != nil and label in js and js[label].kind == JString:
+      js[label].getStr.parse(hubTime, zone = tz)
+    else:
+      now()
+
+  let
+    tz = utc()
+    kind = block:
+      if "head" in js:
+        HubPull
+      elif kind == HubPull:
+        HubIssue
+      else:
+        kind
+
+  case kind
+  of HubRelease:
+    result = HubResult(kind: HubRelease,
+                       created: thenOrNow "created_at",
+                       updated: thenOrNow "updated_at")
+  of HubTag:
+    result = HubResult(kind: HubTag,
+                       tagger: newHubContact(js.getOrDefault("tagger")),
+                       `object`: newHubTree(js.getOrDefault("object")),
+                       created: thenOrNow "created_at",
+                       updated: thenOrNow "updated_at")
+  of HubPull:
+    result = HubResult(kind: HubPull,
+                       created: thenOrNow "created_at",
+                       updated: thenOrNow "updated_at")
+  of HubCode:
+    result = HubResult(kind: HubCode,
+                       created: thenOrNow "created_at",
+                       updated: thenOrNow "updated_at")
+  of HubIssue:
+    result = HubResult(kind: HubIssue,
+                       created: thenOrNow "created_at",
+                       updated: thenOrNow "updated_at")
+  of HubRepo:
+    result = HubResult(kind: HubRepo,
+                       pushed: thenOrNow "pushed_at",
+                       created: thenOrNow "created_at",
+                       updated: thenOrNow "updated_at")
+  of HubCommit:
+    result = HubResult(kind: HubCommit,
+                       commit: newHubCommitMeta(js.getOrDefault("commit")),
+                       created: thenOrNow "created_at",
+                       updated: thenOrNow "updated_at")
+  of HubUser:
+    result = HubResult(kind: HubUser,
+                       created: thenOrNow "created_at",
+                       updated: thenOrNow "updated_at")
+
+  result.htmlUrl = js.get("html_url", "").parseUri
+  result.init(js)
 
 proc newHubGroup*(flags: set[Flag] = defaultFlags): HubGroup =
   result = HubGroup(flags: flags)
