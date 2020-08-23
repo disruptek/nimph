9f1d572d8cd479ad03c9c5276d57a55cae9912d7
Andy Davidoff <andy+github@disruptek.com>
2020-08-22 20:26:20 -0400

    slow down compilation as much as possible

--- nimph/group.nim
+++ nimph/group.nim
@@ -1,3 +1,4 @@
+import std/hashes
 import std/os
 import std/strtabs
 import std/tables
@@ -6,223 +7,215 @@ import std/uri except Url
 
 export strtabs.StringTableMode
 
+import gittyup
+
 import nimph/spec
 import nimph/paths
+import nimph/requirement
+import nimph/version
+
+##[
+
+these are just collection concepts that assert a little more convenience.
+
+a Group is a collection that holds items that may be indexed by some
+stable index. deletion should preserve order whenever possible. singleton
+iteration yields the original item. pairwise iteration also yields unique
+indices that can be used for deletion.
+
+a FlaggedGroup additionally has a flags field/proc that yields set[Flag];
+this is used to alter group operations to, for example, silently omit
+errors and warnings (Quiet) or prevent destructive modification (DryRun).
+
+]##
 
 type
-  Group*[K; V: ref object] = ref object of RootObj
-    table*: OrderedTableRef[K, V]
-    imports*: StringTableRef
-    flags*: set[Flag]
-    mode: StringTableMode
+  Group[T] = concept g
+    add(g, T)                # duplicate indices are accepted
+    incl(g, T)               # do nothing if T's index exists
+    excl(g, T)               # do nothing if T's index does not exist
+    del(g, T)                # delete only T
+    # the rest of these are really trivial expectations we want to meet
+    contains(g, T) is bool
+    hash(T)
+    len(g) is Ordinal
+    for item in items(g):
+      item is T
+    for index, item in pairs(g):
+      item is T
+      del(g, index)
 
-proc init*[K, V](group: Group[K, V]; flags: set[Flag]; mode = modeStyleInsensitive) =
-  ## initialize the table and name cache
-  group.table = newOrderedTable[K, V]()
-  when K is Uri:
-    group.mode = modeCaseSensitive
-  else:
-    group.mode = mode
-  group.imports = newStringTable(group.mode)
-  group.flags = flags
+  ImportGroup*[T] = concept g ##
+    ## an ImportGroup lets you test for membership via ImportName
+    g is Group[T]
+    importName(T) is ImportName
+    contains(g, ImportName) is bool
+    excl(g, ImportName)         # delete any T that yields ImportName
+    `[]`(g, ImportName) is T    # index by ImportName
 
-proc addName[K: string, V](group: Group[K, V]; key: K; value: K) =
-  ## add a name to the group, which points to value
-  assert group.table.hasKey(key)
-  group.imports[key.importName] = value
+  IdentityGroup*[T] = concept g ##
+    ## an IdentityGroup lets you test for membership via Identity,
+    ## PackageName, or Uri
+    g is Group[T]
+    contains(g, Identity) is bool
+    contains(g, PackageName) is bool
+    contains(g, Uri) is bool
+    `[]`(g, PackageName) is T   # indexing by identity types
+    `[]`(g, Uri) is T           # indexing by identity types
+    `[]`(g, Identity) is T      # indexing by identity types
 
-proc addName[K: AbsoluteDir, V](group: Group[K, V]; key: K) =
-  ## add a name to the group, which points to value
-  assert group.table.hasKey(key)
-  group.imports[key.importName] = $key
+  GitGroup*[T] = concept g
+    ## a GitGroup is designed to hold Git objects like tags, references,
+    ## commits, and so on
+    g is Group[T]
+    oid(T) is GitOid
+    contains(g, GitOid) is bool
+    excl(g, GitOid)             # delete any T that yields GitOid
+    `[]`(g, GitOid) is T        # index by GitOid
+    free(T)                     # ensure we can free the group
 
-proc addName[K: Uri, V](group: Group[K, V]; url: K) =
-  ## add a url to the group, which points to value
-  assert group.table.hasKey(url)
-  group.imports[$url] = $url
-  when defined(debug):
-    assert $url.bare notin group.imports
-  group.imports[$url.bare] = $url
+  ReleaseGroup*[T] = concept g
+    ## a ReleaseGroup lets you test for membership via Release,
+    ## (likely Version, Tag, and such as well)
+    g is Group[T]
+    contains(g, Release) is bool
+    for item in g[Release]:     # indexing iteration by Release
+      item is T
 
-proc delName*(group: Group; key: string) =
-  ## remove a name from the group
-  var
-    remove: seq[string]
-  # don't trust anyone; if the value matches, pull the name
-  for name, value in pairs(group.imports):
-    if value == key:
-      remove.add name
-  for name in remove:
-    group.imports.del name
+  FlaggedGroup* = concept g
+    ## this is a container that holds a Group and some contextual
+    ## flags that can affect the Group's operation
+    g.group is Group
+    g.flags is set[Flag]
 
-proc del*[K: string, V](group: Group[K, V]; name: K) =
-  ## remove from the group the named key and its associated value
-  group.table.del name
-  group.delName name
-
-proc del*[K: AbsoluteDir, V](group: Group[K, V]; key: K) =
-  ## remove from the group the named key and its associated value
-  group.table.del key
-  group.delName $key
-
-proc del*[K: Uri, V](group: Group[K, V]; url: K) =
-  ## remove from the group the url key and its associated value
-  group.table.del url
-  group.delName $url
-
-{.warning: "nim bug #12818".}
-proc len*[K, V](group: Group[K, V]): int =
-  ## number of elements in the group
-  result = group.table.len
-
-proc len*(group: Group): int =
-  ## number of elements in the group
-  result = group.table.len
-
-proc get*[K: string, V](group: Group[K, V]; key: K): V =
-  ## fetch a value from the group using style-insensitive lookup
-  if group.table.hasKey(key):
-    result = group.table[key]
-  elif group.imports.hasKey(key.importName):
-    result = group.table[group.imports[key.importName]]
-  else:
-    let emsg = &"{key.importName} not found"
-    raise newException(KeyError, emsg)
-
-proc get*[K: AbsoluteDir, V](group: Group[K, V]; key: K): V =
-  ## fetch a value from the group using a directory
-  if group.table.hasKey(key):
-    result = group.table[key]
-  elif group.imports.hasKey(key.importName):
-    result = group.table[group.imports[key.importName].toAbsoluteDir]
-  else:
-    let emsg = &"{key} not found"
-    raise newException(KeyError, emsg)
-
-proc mget*[K: string, V](group: var Group[K, V]; key: K): var V =
-  ## fetch a mutable value from the group using style-insensitive lookup
-  if group.table.hasKey(key):
-    result = group.table[key]
-  elif group.imports.hasKey(key.importName):
-    result = group.table[group.imports[key.importName]]
-  else:
-    let emsg = &"{key.importName} not found"
-    raise newException(KeyError, emsg)
-
-proc mget*[K: AbsoluteDir, V](group: var Group[K, V]; key: K): var V =
-  ## fetch a mutable value from the group using a directory
-  if group.table.hasKey(key):
-    result = group.table[key]
-  elif group.imports.hasKey(key.importName):
-    result = group.table[group.imports[key.importName].toAbsoluteDir]
-  else:
-    let emsg = &"{key} not found"
-    raise newException(KeyError, emsg)
-
-proc `[]`*[K, V](group: var Group[K, V]; key: K): var V =
-  ## fetch a value from the group using style-insensitive lookup
-  result = group.mget(key)
-
-proc `[]`*[K, V](group: Group[K, V]; key: K): V =
-  ## fetch a value from the group using style-insensitive lookup
-  result = group.get(key)
-
-proc add*[K: string; V](group: Group[K, V]; key: string; value: V) =
-  ## add a key and value to the group
-  if group.table.hasKey(key):
-    group.del key
-  group.table.add key, value
-  group.addName(key.importName, key)
-
-proc add*[K: AbsoluteDir; V](group: Group[K, V]; key: AbsoluteDir; value: V) =
-  ## add a key and value to the group
-  if group.table.hasKey(key):
-    group.del key
-  group.table[key] = value
-  group.addName(key)
-
-proc add*[K: string; V](group: Group[K, V]; url: Uri; value: V) =
-  ## add a (bare) url as a key
-  let
-    naked = url.bare
-    key = $naked
-  if group.table.hasKey(key):
-    group.del key
-  group.table.add key, value
-  # this gets picked up during instant-instantiation of a package from
-  # a project's url, a la asPackage(project: Project): Package ...
-  group.addName naked.importName, key
-
-proc `[]=`*[K, V](group: Group[K, V]; key: K; value: V) =
-  ## set a key to a single value
-  if group.table.hasKey(key):
-    group.del key
-  group.add key, value
-
-{.warning: "nim bug #12818".}
-proc add*[K: Uri, V](group: Group[K, V]; url: Uri; value: V) =
-  ## add a (full) url as a key
-  group.table.add url, value
-  group.addName url
-
-iterator pairs*[K, V](group: Group[K, V]): tuple[key: K; val: V] =
-  ## standard key/value pairs iterator
-  for key, value in group.table.pairs:
-    yield (key: key, val: value)
-
-{.warning: "nim bug #13510".}
-#iterator mpairs*[K, V](group: var Group[K, V]): tuple[key: K; val: var V] =
-iterator mpairs*[K, V](group: Group[K, V]): tuple[key: K; val: var V] =
-  for key, value in group.table.mpairs:
-    #yield (key: key, val: value)
-    yield (key, value)
-
-iterator values*[K, V](group: Group[K, V]): V =
-  ## standard value iterator
-  for value in group.table.values:
-    yield value
-
-iterator keys*[K, V](group: Group[K, V]): K =
-  ## standard key iterator
-  for key in group.table.keys:
-    yield key
-
-iterator mvalues*[K, V](group: var Group[K, V]): var V =
-  ## standard mutable value iterator
-  for value in group.table.mvalues:
-    yield value
-
-proc hasKey*[K, V](group: Group[K, V]; key: K): bool =
-  ## true if the group contains the given key
-  result = group.table.hasKey(key)
-
-proc contains*[K, V](group: Group[K, V]; key: K): bool =
-  ## true if the group contains the given key or its importName
-  result = group.table.contains(key) or group.imports.contains(key.importName)
-
-proc contains*[K, V](group: Group[K, V]; url: Uri): bool =
-  ## true if a member of the group has the same (bare) url
-  for value in group.values:
-    if bareUrlsAreEqual(value.url, url):
-      result = true
+proc contains*[T](group: Group[T]; value: T): bool =
+  for item in items(group):
+    result = item == value
+    if result:
       break
 
-proc contains*[K, V](group: Group[K, V]; value: V): bool =
-  ## true if the group contains the given value
-  for v in group.values:
-    if v == value:
-      result = true
-      break
+proc incl*[T](group: Group[T]; value: T) =
+  if value notin group:
+    group.add value
 
-iterator reversed*[K, V](group: Group[K, V]): V =
-  ## yield values in reverse order of entry
+proc excl*[T](group: Group[T]; value: T) =
+  if value in group:
+    group.del value
+
+proc hash*[T](group: Group[T]): Hash =
+  var h: Hash = 0
+  for item in items(group):
+    h = h !& hash(item)
+  result = !$h
+
+proc contains*[T](flagged: FlaggedGroup[T]; flags: set[Flag]): bool =
+  flags <= flagged.flags
+
+proc contains*[T](flagged: FlaggedGroup[T]; flag: Flag): bool =
+  flag in flagged.flags
+
+proc hash*[T](flagged: FlaggedGroup[T]): Hash = hash(flagged.group)
+
+proc add*[T, V](flagged: FlaggedGroup[T]; value: V) =
+  flagged.group.add value
+
+proc del*[T, V](flagged: FlaggedGroup[T]; value: V) =
+  flagged.group.del value
+
+proc incl*[T, V](flagged: FlaggedGroup[T]; value: V) =
+  flagged.group.incl value
+
+proc excl*[T, V](flagged: FlaggedGroup[T]; value: V) =
+  flagged.group.excl value
+
+proc contains*[T, V](flagged: FlaggedGroup[T]; value: V): bool =
+  value in flagged.group
+
+when false:
+  proc contains*[K, V](group: Group[K, V]; url: Uri): bool =
+    ## true if a member of the group has the same (bare) url
+    for value in group.values:
+      if bareUrlsAreEqual(value.url, url):
+        result = true
+        break
+
+  proc contains*[K, V](group: Group[K, V]; value: V): bool =
+    ## true if the group contains the given value
+    for v in group.values:
+      if v == value:
+        result = true
+        break
+
+iterator reversed*[T](group: Group[T]): T =
+  ## yield values in reverse order
   let
-    elems = toSeq group.values
+    elems = toSeq items(group)
 
   for index in countDown(elems.high, elems.low):
     yield elems[index]
 
-proc clear*[K, V](group: Group[K, V]) =
+proc clear*(group: Group) =
   ## clear the group without any other disruption
-  group.table.clear
-  group.imports.clear(group.mode)
+  while len(group) > 0:
+    for item in items(group):
+      group.del item
+      break
+
+#
+# now our customized implementations...
+#
+proc contains*[T](group: ImportGroup[T]; name: ImportName): bool =
+  for item in items(group):
+    result = item.importName == name
+    if result:
+      break
+
+proc excl*[T](group: ImportGroup[T]; name: ImportName) =
+  while name in group:
+    for item in items(group):
+      if item.importName == name:
+        group.del item
+        break
+
+proc `[]`*[T](group: ImportGroup[T]; name: ImportName): T =
+  if name notin group:
+    raise newException(KeyError, "not found")
+  for item in items(group):
+    if item.importName == name:
+      result = item
+      break
+
+proc free*[T](group: GitGroup[T]) =
+  while len(group) > 0:
+    for item in items(group):
+      group.del item
+      break
+
+proc contains*(group: IdentityGroup; identity: Identity): bool =
+  for item in items(group):
+    result = item == identity
+    if result:
+      break
+
+proc contains*(group: IdentityGroup; name: PackageName): bool =
+  result = newIdentity(name) in group
+
+proc contains*(group: IdentityGroup; url: Uri): bool =
+  result = newIdentity(url) in group
+
+proc add*[T](group: IdentityGroup[T]; value: T) =
+  if value in group:
+    raise newException(KeyError, "duplicates not supported")
+  group.incl value
+
+proc `[]`*[T](group: IdentityGroup[T]; identity: Identity): T =
+  for item in items(group):
+    if item == identity:
+      result = item
+      break
+
+proc `[]`*[T](group: IdentityGroup[T]; url: Uri): T =
+  result = group[newIdentity(url)]
+
+proc `[]`*[T](group: IdentityGroup[T]; name: PackageName): T =
+  result = group[newIdentity(name)]
