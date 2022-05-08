0b63f47b47943f4fcf1c209c33ffc04d2a44a6b8
Smooth Operator <disruptek@users.noreply.github.com>
2022-05-08 16:39:16 -0400

    hax whatfer supporting nimskull maybe (#159)
    * hax whatfer supporting nimskull maybe
    
    * do it even more transparently, omit exports

---
+++ src/nimph/skullduggery.nim
@@ -0,0 +1,26 @@
+#[
+
+yeah, it's a hack and pulls gratuitous symbols into everything.
+while we support mainline nim, it's a price i'm willing to pay
+for not having to read this crap at the top of every file...
+
+]#
+
+when (compiles do: import compiler/utils/pathutils):
+  # nimskull
+  import compiler/ast/ast
+  import compiler/ast/idents
+  import compiler/ast/lineinfos
+  import compiler/front/options as compileropts
+  import compiler/front/nimconf
+  import compiler/front/condsyms
+  import compiler/utils/pathutils
+else:
+  # mainline nim
+  import compiler/ast
+  import compiler/idents
+  import compiler/nimconf
+  import compiler/options as compileropts
+  import compiler/pathutils
+  import compiler/condsyms
+  import compiler/lineinfos
