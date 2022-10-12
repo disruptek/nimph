a05f0acec76eab06e8ecd68f98072c4742ca03a7
Smooth Operator <github@andy.disruptek.com>
2022-10-12 14:02:34 -0400

    update and simplify the bootstrap

--- bootstrap-nonimble.sh
+++ bootstrap-nonimble.sh
@@ -9,29 +9,25 @@ if [ "$GITHUB_ACTIONS" = "true" ]; then
   fi
   SO="$(pwd)/libgit2/build/libgit2.$LGEXT"
   ls -ld "$SO"
-  PASSES="--define:libgit2Lib=$SO --passC:\"-I$(pwd)/libgit2/include\" --define:nimDebugDlOpen"
+  PASSES="--passC:\"-I$(pwd)/libgit2/include\" --define:nimDebugDlOpen"
 fi
 
 mkdir -p temporary
 cd temporary
 
-git clone --depth 1 --branch 1.8.29 https://github.com/disruptek/bump.git
-git clone --depth 1 --branch 1.1.2 https://github.com/disruptek/cutelog.git
+git clone --depth 1 --branch 1.8.31 https://github.com/disruptek/bump.git
+git clone --depth 1 --branch 2.0.0 https://github.com/disruptek/cutelog.git
 git clone --depth 1 --branch 3.1.0 https://github.com/disruptek/gittyup.git
-git clone --depth 1 --branch v0.6.13 https://github.com/genotrance/nimterop.git
-git clone --depth 1 --branch v1.5.19 https://github.com/c-blake/cligen.git
+git clone --depth 1 --branch 2.0.0 https://github.com/disruptek/cligen.git
 git clone --depth 1 --branch 0.26.0 https://github.com/zevv/npeg.git
 git clone --depth 1 --branch 1.0.2 https://github.com/disruptek/jsonconvert.git
 git clone --depth 1 --branch 2.1.3 https://github.com/disruptek/badresults.git
 git clone --depth 1 --branch 2.0.4 https://github.com/disruptek/github.git
-git clone --depth 1 --branch 1.0.5 https://github.com/disruptek/rest.git
-git clone --depth 1 --branch 1.0.2 https://github.com/disruptek/foreach.git
 git clone --depth 1 --branch 0.0.7 https://github.com/disruptek/ups.git
-git clone --depth 1 --branch 0.5.3 https://github.com/disruptek/grok.git
 git clone --depth 1 --branch 0.1.6 https://github.com/haxscramper/hlibgit2.git
 git clone --depth 1 --branch 0.1.5 https://github.com/haxscramper/hlibssh2.git
 
-nim c --outdir:.. --define:release --path:../src --path:hlibgit2/src --path:hlibssh2/src --path:ups --path:cligen --path:foreach --path:github/src --path:rest --path:npeg/src --path:jsonconvert --path:badresults --path:bump --path:cutelog --path:gittyup --path:grok $PASSES ../src/nimph.nim
+nim c --outdir:.. --define:release --path:../src --path:hlibgit2/src --path:hlibssh2/src --path:ups --path:cligen --path:github/src --path:npeg/src --path:jsonconvert --path:badresults --path:bump --path:cutelog --path:gittyup $PASSES ../src/nimph.nim
 cd ..
 
 if test -x nimph; then
