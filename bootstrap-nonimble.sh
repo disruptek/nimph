7a74b24ee6bc6b5bd805a37bc6abad1f3e4374b9
Leorize <leorize+oss@disroot.org>
2023-11-05 15:09:56 -0600

    bootstrap: switch to alaviss fork of hlibgit2
    This fixes a memory corruption due to ABI error in libgit2

--- bootstrap-nonimble.sh
+++ bootstrap-nonimble.sh
@@ -24,7 +24,7 @@ git clone --depth 1 --branch 1.0.2 https://github.com/disruptek/jsonconvert.git
 git clone --depth 1 --branch 2.1.3 https://github.com/disruptek/badresults.git
 git clone --depth 1 --branch 3.0.0 https://github.com/disruptek/github.git
 git clone --depth 1 --branch 0.0.7 https://github.com/disruptek/ups.git
-git clone --depth 1 --branch 0.1.6 https://github.com/haxscramper/hlibgit2.git
+git clone --depth 1 --branch 0.1.7 https://github.com/alaviss/hlibgit2.git
 git clone --depth 1 --branch 0.1.5 https://github.com/haxscramper/hlibssh2.git
 
 nim c --outdir:.. --define:release --path:../src --path:hlibgit2/src --path:hlibssh2/src --path:ups --path:cligen --path:github --path:npeg/src --path:jsonconvert --path:badresults --path:bump --path:cutelog --path:gittyup $PASSES ../src/nimph.nim
